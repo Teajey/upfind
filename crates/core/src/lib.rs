fef86fd489827d82aeea60a036d99c822f57311d
Teajey <21069848+Teajey@users.noreply.github.com>
2025-11-07 11:32:38 +1300

    Prevent matching on special directory entries

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,4 +1,7 @@
-use std::{ffi::OsString, path::Path};
+use std::{
+    ffi::OsString,
+    path::{Path, PathBuf},
+};
 
 struct Parents<'a> {
     current: Option<&'a Path>,
@@ -34,20 +37,29 @@ pub enum Error {
 pub fn iter<P>(
     starting_directory: &Path,
     sub_paths_to_match: &[P],
-) -> impl Iterator<Item = Result<glob::Paths, Error>>
+) -> impl Iterator<Item = Result<impl Iterator<Item = Result<PathBuf, glob::GlobError>>, Error>>
 where
     P: AsRef<Path>,
 {
     starting_directory.parents().flat_map(move |directory| {
-        sub_paths_to_match
-            .iter()
-            .map(|p| -> Result<glob::Paths, Error> {
-                let full_path = directory.join(p);
-                let os_str = full_path.as_os_str();
-                let string = os_str.to_str().ok_or_else(|| Error::OsStrConversionFail {
-                    original_string: os_str.to_os_string(),
-                })?;
-                glob::glob(string).map_err(Error::Pattern)
+        sub_paths_to_match.iter().map(|p| {
+            let full_path = directory.join(p);
+            let os_str = full_path.as_os_str();
+            let string = os_str.to_str().ok_or_else(|| Error::OsStrConversionFail {
+                original_string: os_str.to_os_string(),
+            })?;
+            glob::glob(string).map_err(Error::Pattern).map(|paths| {
+                paths.filter(|result| {
+                    result
+                        .as_ref()
+                        .map(|path| {
+                            path.as_os_str()
+                                .to_str()
+                                .is_none_or(|name| !name.ends_with("/.") && !name.ends_with("/.."))
+                        })
+                        .unwrap_or(true)
+                })
             })
+        })
     })
 }
